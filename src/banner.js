afc32f633ea5c50876d6e86892586100d31a0d60
Gunbam27 <mingmi_27@naver.com>
2022-04-28 22:21:40 +0900

    04.28
    구조 CSS 다듬기 및 canvas innerwidth 비율

--- src/banner.js
+++ src/banner.js
@@ -1,21 +1,51 @@
-
-// 오른쪽 큰원
 const br = document.getElementById('bigRound');
 const brctx = br.getContext('2d');
+const sr = document.getElementById('smallRound');
+const srctx = sr.getContext('2d');
+const tr = document.getElementById('twoRound');
+const trctx = tr.getContext('2d');
+const bnimg =document.querySelector('.bnimg')
+bigRound();
+smallRound();
+twoRound();
+
+window.onresize=(function(){
+    if(window.innerWidth<1440){
+    br.width = window.innerWidth;
+    // br.height = window.innerWidth;
+    sr.width = Math.floor((window.innerWidth/250)*100);
+    tr.width = Math.floor((window.innerWidth/400)*100);
+    bigRound();
+    smallRound();
+    twoRound();
+}else {
+    br.width = 1395;
+    sr.width = 782;
+    tr.width = 320;
+    bigRound();
+    smallRound();
+    twoRound();
+}
+});
+
+// 오른쪽 큰원
+function bigRound(){
 brctx.beginPath();
 brctx.arc(1100, 450, 700, 0, 2*Math.PI);
 brctx.fillStyle = '#FEEEDA';
 brctx.fill();
+}
+
 // 오른쪽 작은원
-const sr = document.getElementById('smallRound');
-const srctx = sr.getContext('2d');
+function smallRound(){
 srctx.beginPath();
 srctx.arc(490, 430, 400, 0, 2*Math.PI);
 srctx.fillStyle = '#EE7F83';
 srctx.fill();
+}
 // 왼쪽 위 작은원 & 아래작은원
-const tr = document.getElementById('twoRound');
-const trctx = tr.getContext('2d');
+function twoRound(){
+// 왼쪽 위 작은원
 trctx.beginPath();
 trctx.arc(140,75, 70, 0, 2*Math.PI);
 trctx.fillStyle = '#FEEEDA';
@@ -25,6 +55,10 @@ trctx.beginPath();
 trctx.arc(100, 150, 100, 0, 2*Math.PI);
 trctx.fillStyle = '#FEEEDA';
 trctx.fill();
+}
+
+    
+
 
 
 $(()=>{
