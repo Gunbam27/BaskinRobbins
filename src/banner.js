23b16e71d57e2c1e92b4812e56788d5a1a403b24
Gunbam27 <mingmi_27@naver.com>
2022-05-09 22:53:18 +0900

    05.09
    indi css

--- src/banner.js
+++ src/banner.js
@@ -1,3 +1,8 @@
+// Banner Slide
+const text_slide =document.querySelector('.banner__slide');
+const image_slide =document.querySelector('.banner__image-slide');
+const bg_slide =document.querySelector('.banner__bg');
+// Canvas
 const br = document.getElementById('bigRound');
 const brctx = br.getContext('2d');
 const sr = document.getElementById('smallRound');
@@ -8,71 +13,18 @@ const bnimg =document.querySelector('.bnimg')
 let colornum = 0;
 let colornum_Tr =0;
 let colornum_Sr=0;
+let color_Indi =1;
 var color = ['#FEEEDA','#EB9B22','#643725','#5AE9C0','#EE7F83']
 var colorBg = ['#EE7F83','#FEEEDA','#EB9B22','#643725','#5AE9C0']
 
-
-
-window.onresize=(function(){
-    if(window.innerWidth<1440){
-    br.width = window.innerWidth;
-    // br.height = window.innerWidth;
-    sr.width = Math.floor((window.innerWidth/250)*100);
-    tr.width = Math.floor((window.innerWidth/400)*100);
-    bigRound();
-    smallRound();
-    twoRound();
-}else {
-    br.width = 1395;
-    sr.width = 782;
-    tr.width = 320;
-    bigRound();
-    smallRound();
-    twoRound();
-}
-});
-
-
-// 오른쪽 큰원
-function bigRound(){
-    if(colornum===5){colornum=0};
-    brctx.clearRect(0,0,br.width,br.height);
-    brctx.beginPath();
-    brctx.arc(1100, 450, 700, 0, 2*Math.PI);
-    brctx.fillStyle = color[colornum];
-    brctx.fill();
-    colornum+=1;
-}
-// 오른쪽 작은원
-function smallRound(){
-    if(colornum_Sr===5){colornum_Sr=0};
-    srctx.clearRect(0,0,sr.width,sr.height);
-    srctx.beginPath();
-    srctx.arc(480, 400, 430, 0, 2*Math.PI);
-    srctx.fillStyle = colorBg[colornum_Sr];
-    srctx.fill();
-    colornum_Sr+=1;
-}
-// 왼쪽 위 작은원 & 아래작은원
-function twoRound(){
-    // 왼쪽 위 작은원
-    if(colornum_Tr===5){colornum_Tr=0};
-    trctx.clearRect(0,0,tr.width,tr.height);
-    trctx.beginPath();
-    trctx.arc(140,75, 70, 0, 2*Math.PI);
-    // 왼쪽 아래 큰원
-    trctx.arc(100, 150, 100, 0, 2*Math.PI);
-    trctx.fillStyle = color[colornum_Tr];
-    trctx.fill();
-    colornum_Tr+=1;
-}
+const fontColor = document.querySelectorAll('.banner__indicator span');
+console.log(fontColor);
 
 
 
-const text_slide =document.querySelector('.banner__slide');
-const image_slide =document.querySelector('.banner__image-slide');
-const bg_slide =document.querySelector('.banner__bg');
-window.addEventListener('load', () =>{
+
+
+window.addEventListener('load', (e) =>{
     bigRound();
     smallRound();
     twoRound();
@@ -113,9 +65,51 @@ window.addEventListener('load', () =>{
         }
 
     const autoCall = () => {
-        autoI = setInterval(() => {slideText(),slideImage(),bigRound(),smallRound(),twoRound(),slideBg()}, 5000);
+        autoI = setInterval(() => {slideText(),slideImage(),indiColor(),bigRound(),smallRound(),twoRound(),slideBg()}, 5000);
     };
     autoCall();
 })
 
 
+// 오른쪽 큰원
+function bigRound(){
+    if(colornum===5){colornum=0};
+    brctx.clearRect(0,0,br.width,br.height);
+    brctx.beginPath();
+    brctx.arc(1100, 450, 700, 0, 2*Math.PI);
+    brctx.fillStyle = color[colornum];
+    brctx.fill();
+    colornum+=1;
+}
+// 오른쪽 작은원
+function smallRound(){
+    if(colornum_Sr===5){colornum_Sr=0};
+    srctx.clearRect(0,0,sr.width,sr.height);
+    srctx.beginPath();
+    srctx.arc(480, 400, 430, 0, 2*Math.PI);
+    srctx.fillStyle = colorBg[colornum_Sr];
+    srctx.fill();
+    colornum_Sr+=1;
+}
+// 왼쪽 위 작은원 & 아래작은원
+function twoRound(){
+    // 왼쪽 위 작은원
+    if(colornum_Tr===5){colornum_Tr=0};
+    trctx.clearRect(0,0,tr.width,tr.height);
+    trctx.beginPath();
+    trctx.arc(140,75, 70, 0, 2*Math.PI);
+    // 왼쪽 아래 큰원
+    trctx.arc(100, 150, 100, 0, 2*Math.PI);
+    trctx.fillStyle = color[colornum_Tr];
+    trctx.fill();
+    colornum_Tr+=1;
+}
+// 인디케이터 제목 색
+function indiColor(){
+    if(color_Indi===5){color_Indi=0};
+    fontColor[color_Indi].style.color = color[color_Indi];
+    setTimeout(() => {
+        fontColor[color_Indi-1].style.color = '#666';
+    }, 4900); 
+    color_Indi+=1;
+}
